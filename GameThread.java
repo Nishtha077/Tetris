c6bd77dcce8b49fc6506b7cd88866d195f0548f6
himverto25 <121309034+himverto25@users.noreply.github.com>
2023-03-29 19:36:44 +0530

    Added Score Label

--- GameThread.java
+++ GameThread.java
@@ -6,9 +6,16 @@ import java.util.logging.Logger;
 public class GameThread extends Thread
 {   
     private GameArea ga;
-    public GameThread(GameArea ga)
+    private GameForm gf;
+    private int score;
+    private int level=1;
+    private int scorePerLevel=3;
+    private int pause = 1000;
+    private int speedupPerLevel=100;
+    public GameThread(GameArea ga, GameForm gf)
     {
         this.ga=ga;
+        this.gf=gf;
     }
     @Override
     public void run()
@@ -20,12 +27,27 @@ public class GameThread extends Thread
             {
             try 
             {
-                Thread.sleep(500);
+                Thread.sleep(pause);
             } 
             catch (InterruptedException ex) 
             {
                 Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
             }
+            }
+            if(ga.isBlockOutOfBounds())
+            {
+                break;
+            }
+            ga.moveBlockToBackground();
+            ga.clearLines();
+            score+=ga.clearLines();
+            gf.updateScore(score);
+            int lvl = score/scorePerLevel + 1;
+            if(lvl>level)
+            {
+                level=lvl;
+                gf.updateLevel(level);
+                pause -= speedupPerLevel;
             }
         }
     }
